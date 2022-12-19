9e55b94ae566fb1b5cd419006d1e6d34e3c75cf4
Apricitine <apricitinecas@gmail.com>
2022-12-19 14:50:34 -0800

    Remove Isaac, Add Storyblok Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,29 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { loadEnv } from "vite";
 
 // https://astro.build/config
+import storyblok from "@storyblok/astro";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), partytown(), tailwind()]
+  site: "https://blockheads-5921.vercel.app",
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown(),
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {},
+      apiOptions: {
+        region: "us",
+      },
+    }),
+  ],
 });
