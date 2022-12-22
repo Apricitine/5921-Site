eeeae8db9f7897981fabe95106510f214c82d2ea
Apricitine <apricitinecas@gmail.com>
2022-12-21 19:34:24 -0800

    Weirddd Stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,25 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 /* import storyblok from '@storyblok/astro'; */
+import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://blockheads-5921.vercel.app",
+  output: "server",
   integrations: [
     partytown(),
     tailwind(),
-/*     storyblok({
-      accessToken: process.env.STORYBLOK_TOKEN,
-      apiOptions: {
-        region: "us"
-      },
-      components: {
-        docsPost: 'storyblok/DocsPost',
-        docsPostList: 'storyblok/DocsPostLight',
-        page: 'storyblok/Page',
-      }
-    }) */
+    /*     storyblok({
+        accessToken: process.env.STORYBLOK_TOKEN,
+        apiOptions: {
+          region: "us"
+        },
+        components: {
+          docsPost: 'storyblok/DocsPost',
+          docsPostList: 'storyblok/DocsPostLight',
+          page: 'storyblok/Page',
+        }
+      }) */ svelte(),
   ],
 });
