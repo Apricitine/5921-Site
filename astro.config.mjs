8781f41ddb2a92c924aa8ffa952f028811a696e8
Apricitine <apricitinecas@gmail.com>
2022-12-21 18:24:46 -0800

    Try?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,27 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { loadEnv } from "vite";
 
 // https://astro.build/config
-import storyblok from "@storyblok/astro";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
+import storyblok from '@storyblok/astro';
 
-const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blockheads-5921.vercel.app",
   integrations: [
-    mdx(),
-    sitemap(),
     partytown(),
     tailwind(),
     storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        docsPost: "storyblok/DocsPost",
-        docsPostList: "storyblok/DocsPostList",
-        page: "storyblok/Page",
-      },
+      accessToken: process.env.STORYBLOK_TOKEN,
       apiOptions: {
-        region: "us",
-      },
-    }),
+        region: "us"
+      }
+      components: {
+        docsPost: 'storyblok/DocsPost',
+        docsPostList: 'storyblok/DocsPostLight',
+        page: 'storyblok/Page',
+      }
+    })
   ],
 });
