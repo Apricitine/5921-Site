30b8d5045255ed1e44f39176d088a78d96aa83a2
Apricitine <apricitinecas@gmail.com>
2022-12-21 18:29:46 -0800

    Nope

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,25 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+// integrations
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
-import storyblok from '@storyblok/astro';
+/* import storyblok from '@storyblok/astro'; */
 
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://blockheads-5921.vercel.app",
   integrations: [
     partytown(),
     tailwind(),
-    storyblok({
+/*     storyblok({
       accessToken: process.env.STORYBLOK_TOKEN,
       apiOptions: {
         region: "us"
-      }
+      },
       components: {
         docsPost: 'storyblok/DocsPost',
         docsPostList: 'storyblok/DocsPostLight',
         page: 'storyblok/Page',
       }
-    })
+    }) */
   ],
 });
--- src/pages/rss.xml.js
+++
@@ -1,9 +0,0 @@
-import rss from '@astrojs/rss';
-import { SITE_TITLE } from '../config';
-
-export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	});
