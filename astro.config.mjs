30454988c0fb238cfe4f569a89e05d913555c3c6
Apricitine <apricitinecas@gmail.com>
2023-01-28 19:22:50 -0800

    update vercel adapter (for some reason)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,22 +7,24 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://blockheads-5921.vercel.app",
   output: "server",
-  integrations: [
-    partytown(),
-    tailwind(),
-    /*     storyblok({
-        accessToken: process.env.STORYBLOK_TOKEN,
-        apiOptions: {
-          region: "us"
-        },
-        components: {
-          docsPost: 'storyblok/DocsPost',
-          docsPostList: 'storyblok/DocsPostLight',
-          page: 'storyblok/Page',
-        }
-      }) */ svelte(),
-  ],
+  integrations: [partytown(), tailwind(),
+  /*     storyblok({
+      accessToken: process.env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "us"
+      },
+      components: {
+        docsPost: 'storyblok/DocsPost',
+        docsPostList: 'storyblok/DocsPostLight',
+        page: 'storyblok/Page',
+      }
+    }) */
+  svelte()],
+  adapter: vercel()
 });
